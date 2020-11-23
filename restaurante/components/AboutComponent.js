08d2cb6d39197d290739abf30b04290cc28213a5
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-23 07:05:43 -0500

    indicador de actividad, loading

--- restaurante/components/AboutComponent.js
+++ restaurante/components/AboutComponent.js
@@ -3,6 +3,7 @@ import { FlatList, Text, ScrollView } from 'react-native';
 import { ListItem, Avatar, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -65,14 +66,38 @@ class About extends Component {
     static navigationOptions = {
         title: 'About Us'
     };
-    debugger;
+    
     render() {
-       return (
-        <ScrollView>
-            <History/>
-            <Leadership leaders={this.props.leaders.leaders}/>
-        </ScrollView>
-       );
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return (
+                <ScrollView>
+                    <History/>
+                    <Leadership leaders={this.props.leaders.leaders}/>
+                </ScrollView>
+            );
+        }
     }
 }
 
