6f373cc669e4c5c08bc79359406634a31aa89de7
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-26 06:54:07 -0500

    se adiciona metodo para eliminar un favorito

--- restaurante/components/FavoriteComponent.js
+++ restaurante/components/FavoriteComponent.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, TouchableOpacity, Animated, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
       favorites: state.favorites
     }
-  }
+}
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})
 
 class Favorites extends Component {
 
@@ -19,20 +25,55 @@ class Favorites extends Component {
     };
 
     render() {
-       
         const renderMenuItem = ({item, index}) => {
-    
-            return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
-                    />
+            const rightButton = (progress, dragX) => {
+                const scale = dragX.interpolate({
+                    inputRange: [-100, 0],
+                    outputRange: [0.7, 0]
+                })
+                return(
+                    <>
+                        <TouchableOpacity onPress={() =>{ 
+                            Alert.alert(
+                                'Delete Favorite?',
+                                'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                                [
+                                    { 
+                                        text: 'Cancel', 
+                                        onPress: () => console.log(item.name + 'Not Deleted'),
+                                        style: ' cancel'
+                                    },
+                                    {
+                                        text: 'OK',
+                                        onPress: () => this.props.deleteFavorite(item.id)
+                                    }
+                                ],
+                                { cancelable: false }
+                            );
+                        }}>
+                            <View style={{flex:1, backgroundColor: 'red', justifyContent: 'center'}}>
+                                <Animated.Text style={{color: 'white', paddingHorizontal: 10,
+                                        fontWeight:'600', transform: [{scale}]}}>
+                                            Delete
+                                </Animated.Text>
+                            </View>
+                        </TouchableOpacity>
+                    </>
+                );
+            }
+            return(
+                <Swipeable renderRightActions={rightButton}>
+                 <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                        />
+                </Swipeable>
             );
-        };
+        }
 
         const { navigate } = this.props.navigation;
 
@@ -61,4 +102,4 @@ class Favorites extends Component {
 }
 
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
