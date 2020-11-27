35892f697304f6d0befb2083e0bdd222be89e041
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-26 21:16:36 -0500

    se ajusta dishdetail y el modulo de reservas

--- restaurante/components/ReservationComponent.js
+++ restaurante/components/ReservationComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, TouchableOpacity, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Moment from 'moment';
+import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
 
@@ -29,7 +30,16 @@ class Reservation extends Component {
 
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        //this.toggleModal();
+
+        Alert.alert(
+            '','You Reservation OK?\n\nNumber of Guests: '+this.state.guests+'\nSmoking? '+(this.state.smoking?'Yes':'No')+'\nDate and Time: '+this.state.date,
+            [
+            {text: 'Cancel', onPress: () => {this.resetForm()}, style: 'cancel'},
+            {text: 'OK', onPress: () =>  {this.resetForm() }}
+            ],
+            { cancelable: false }
+        );
     }
 
     resetForm() {
@@ -48,78 +58,80 @@ class Reservation extends Component {
 
         return(
             <ScrollView>
-                <Card>
-                    <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Number of Guests</Text>
-                    <Picker
-                        style={styles.formItem}
-                        selectedValue={this.state.guests}
-                        onValueChange={(itemValue, itemIndex) => this.setState({guests: itemValue})}>
-                        <Picker.Item label="1" value="1" />
-                        <Picker.Item label="2" value="2" />
-                        <Picker.Item label="3" value="3" />
-                        <Picker.Item label="4" value="4" />
-                        <Picker.Item label="5" value="5" />
-                        <Picker.Item label="6" value="6" />
-                    </Picker>
-                    </View>
-                    <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
-                    <Switch
-                        style={styles.formItem}
-                        value={this.state.smoking}
-                        onTintColor='#512DA8'
-                        onValueChange={(value) => this.setState({smoking: value})}>
-                    </Switch>
-                    </View>
-                    
-                    <View style={styles.formRow}>
-                        <Text style={styles.formLabel}>Date and Time</Text>
-                        <TouchableOpacity style={styles.formItem}
-                                style={{
-                                    padding: 7,
-                                    borderColor: '#512DA8',
-                                    borderWidth: 2,
-                                    flexDirection: "row"
-                                }}
-                                onPress={() => this.setState({ show: true, mode: 'date' })}>
-                            <Icon type='font-awesome' name='calendar' color='#512DA8' />
-                            <Text >
-                                {' ' + Moment(this.state.date).format('DD-MMM-YYYY h:mm A') }
-                            </Text>
-                        </TouchableOpacity>
-                        {/* Date Time Picker */}
-                        {this.state.show && (
-                            <DateTimePicker
-                                value={this.state.date}
-                                mode={this.state.mode}
-                                minimumDate={new Date()}
-                                minuteInterval={30}
-                                onChange={(event, date) => {
-                                    if (date === undefined) {
-                                        this.setState({ show: false });
-                                    }
-                                    else {
-                                        this.setState({
-                                            show: this.state.mode === "time" ? false : true,
-                                            mode: "time",
-                                            date: new Date(date)
-                                        });
-                                    }
-                                }}
+                <Animatable.View animation="zoomInUp" duration={2000} delay={1000}>
+                    <Card>
+                        <View style={styles.formRow}>
+                        <Text style={styles.formLabel}>Number of Guests</Text>
+                        <Picker
+                            style={styles.formItem}
+                            selectedValue={this.state.guests}
+                            onValueChange={(itemValue, itemIndex) => this.setState({guests: itemValue})}>
+                            <Picker.Item label="1" value="1" />
+                            <Picker.Item label="2" value="2" />
+                            <Picker.Item label="3" value="3" />
+                            <Picker.Item label="4" value="4" />
+                            <Picker.Item label="5" value="5" />
+                            <Picker.Item label="6" value="6" />
+                        </Picker>
+                        </View>
+                        <View style={styles.formRow}>
+                        <Text style={styles.formLabel}>Smoking/Non-Smoking?</Text>
+                        <Switch
+                            style={styles.formItem}
+                            value={this.state.smoking}
+                            onTintColor='#512DA8'
+                            onValueChange={(value) => this.setState({smoking: value})}>
+                        </Switch>
+                        </View>
+                        
+                        <View style={styles.formRow}>
+                            <Text style={styles.formLabel}>Date and Time</Text>
+                            <TouchableOpacity style={styles.formItem}
+                                    style={{
+                                        padding: 7,
+                                        borderColor: '#512DA8',
+                                        borderWidth: 2,
+                                        flexDirection: "row"
+                                    }}
+                                    onPress={() => this.setState({ show: true, mode: 'date' })}>
+                                <Icon type='font-awesome' name='calendar' color='#512DA8' />
+                                <Text >
+                                    {' ' + Moment(this.state.date).format('DD-MMM-YYYY h:mm A') }
+                                </Text>
+                            </TouchableOpacity>
+                            {/* Date Time Picker */}
+                            {this.state.show && (
+                                <DateTimePicker
+                                    value={this.state.date}
+                                    mode={this.state.mode}
+                                    minimumDate={new Date()}
+                                    minuteInterval={30}
+                                    onChange={(event, date) => {
+                                        if (date === undefined) {
+                                            this.setState({ show: false });
+                                        }
+                                        else {
+                                            this.setState({
+                                                show: this.state.mode === "time" ? false : true,
+                                                mode: "time",
+                                                date: new Date(date)
+                                            });
+                                        }
+                                    }}
+                                />
+                            )}
+                        </View>
+                        
+                        <View style={styles.formRow}>
+                        <Button
+                            onPress={() => this.handleReservation()}
+                            title="Reserve"
+                            color="#512DA8"
+                            accessibilityLabel="Learn more about this purple button"
                             />
-                        )}
-                    </View>
-                    
-                    <View style={styles.formRow}>
-                    <Button
-                        onPress={() => this.handleReservation()}
-                        title="Reserve"
-                        color="#512DA8"
-                        accessibilityLabel="Learn more about this purple button"
-                        />
-                    </View>
-                </Card>
+                        </View>
+                    </Card>
+                </Animatable.View>
 
                 <Modal animationType = {"slide"} transparent = {false}
                     visible = {this.state.showModal}
