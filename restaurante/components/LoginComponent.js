0ec5c968fcf72d069293cddd39fe9b068ab16ce5
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-11-27 22:42:14 -0500

    convertir imagen a png y bajar resolucion

--- restaurante/components/LoginComponent.js
+++ restaurante/components/LoginComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Image, ScrollView } from 'react-native';
-import { Card, Icon, Input, CheckBox, Button } from 'react-native-elements';
+import { Icon, Input, CheckBox, Button } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import { Asset } from 'expo-asset';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 class LoginTab extends Component {
 
@@ -20,7 +22,6 @@ class LoginTab extends Component {
     }
 
     componentDidMount() {
-        /**/
         SecureStore.getItemAsync('userinfo')
             .then((userdata) => {
                 console.log('a: ',JSON.stringify(userdata));
@@ -150,9 +151,22 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri });
+                //this.setState({imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri, 
+            [
+                {resize: {width: 400}}
+            ],
+            {format: 'png'}
+        ).catch((error) => console.log('error process image', error));
+        console.log('processImage:',processedImage);
+        this.setState({imageUrl: processedImage.uri });
 
     }
     
